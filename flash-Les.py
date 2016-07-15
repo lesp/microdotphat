c3aaa0f1fc13e39385240762ea667230b1889fd3
Les Pounder <lespounder@gmail.com>
2016-07-15 16:08:25 +0000

    Updated flash-Les with extra blinky

--- flash-Les.py
+++ flash-Les.py
@@ -5,7 +5,7 @@ import math
 from microdotphat import clear, show, set_pixel, WIDTH, HEIGHT
 from random import randint
 
-t = 0.01
+t = 0.1
 print WIDTH
 
 while True:
@@ -30,22 +30,43 @@ while True:
     clear()
     show()
     time.sleep(t)
-    for x in range(45):
+    for x in range(6):
         set_pixel(x,0,1)
         set_pixel(x,2,1)
         set_pixel(x,4,1)
-        set_pixel(x,6,1)       
+        set_pixel(x,6,1)
         show()
-        """
-        for y in range(6):
-            set_pixel(rand_x1,rand_y1,1)
-            set_pixel(rand_x2,rand_y2,1)
-            set_pixel(rand_x3,rand_y3,1)
-            set_pixel(rand_x4,rand_y4,1)
-            set_pixel(rand_x5,rand_y5,1)
-            set_pixel(rand_x6,rand_y6,1)
-            set_pixel(rand_x7,rand_y7,1)
-            set_pixel(rand_x8,rand_y8,1)
-    show()
-    """
+       
+    for x in range(6,13):
+        set_pixel(x,0,1)
+        set_pixel(x,2,1)
+        set_pixel(x,4,1)
+        set_pixel(x,6,1)
+        show()
+    
+    for x in range(13,21):
+        set_pixel(x,0,1)
+        set_pixel(x,2,1)
+        set_pixel(x,4,1)
+        set_pixel(x,6,1)
+        show()
+    for x in range(21,29):
+        set_pixel(x,0,1)
+        set_pixel(x,2,1)
+        set_pixel(x,4,1)
+        set_pixel(x,6,1)
+        show()
+    
+    for x in range(29,37):
+        set_pixel(x,0,1)
+        set_pixel(x,2,1)
+        set_pixel(x,4,1)
+        set_pixel(x,6,1)
+        show()
+    for x in range(37,45):
+        set_pixel(x,0,1)
+        set_pixel(x,2,1)
+        set_pixel(x,4,1)
+        set_pixel(x,6,1)
+        show()
     time.sleep(t)
